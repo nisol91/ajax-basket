fe48b5c1e853ca29659c5da0d0030cbcc77fb4b7
nisol91 <nicolasolzi91@gmail.com>
2018-12-12 15:35:00 +0100

    3

--- basket_ajax.js
+++ basket_ajax.js
@@ -7,20 +7,23 @@ $('.my_button').click(function() {
     $.ajax({
     url: 'https://www.boolean.careers/api/array/basket',
     type: 'GET',
+    //ho 2 alternative: o scrivere n=valore dopo basket nell url, oppure aggiungere a data qui sotto il n: valore.
     data: {
       n: valore,
     },
     success: function(data) {
       console.log("success");
       //handlebars-----
+      //dopo aver generato gli n giocatori, per n volte li appendo nella sidebar
       for (var i = 0; i < valore ; i++) {
       var template_base = $('#my-template').html();
       var template = Handlebars.compile(template_base);
 
-      console.log(data.response[i]['playerCode']);
       var context = {
         codice: data.response[i]['playerCode'],
       };
+      console.log(data.response[i]['playerCode']);
+      //questo qui sopra e' esattamente la posizione del codice giocatore nel DB generato nella API
 
       var html = template(context);
 
@@ -45,6 +48,11 @@ $('.my_button').click(function() {
             var template = Handlebars.compile(template_base);
 
             var html_2 = template(mioGioc);
+            //mioGioc sono i valori delle statistiche (ovvero le proprieta dell oggetto), proprio
+            //per quel particlare giocatore (ovvero quel particolare data.response[i]), che ha proprio
+            //il codice che ho cliccato nella sidebar.
+            //li vado a richiamare nell html dandogli il nome della singola proprieta( in questo caso
+            //points, fouls...ecc)
             console.log(html_2);
 
             $('.contMail .entry').hide();
